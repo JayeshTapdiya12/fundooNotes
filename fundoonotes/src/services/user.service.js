05b3caa11cf62983e74ef07a22d3a7e412fb2111
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-07-16 11:48:04 +0530

    created the login and sign up with bcrypt .js to secure the password

--- fundoonotes/src/services/user.service.js
+++ fundoonotes/src/services/user.service.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model';
+import bcrypt from "bcrypt";
 
 
 export const getData = async () => {
@@ -8,21 +9,36 @@ export const getData = async () => {
 
 //create new user
 export const newUser = async (body) => {
+
+  const { name, lname, email, password } = body
   const exist = await User.findOne({ email: body.email })
+
   if (exist) {
     throw new Error("User already exist ")
   } else {
-    const data = await User.create(body);
+
+    // using bcrypt
+    const saltRounds = 10;
+    const hash_password = await bcrypt.hash(password, saltRounds)
+
+    // creatng a new user
+    const data = await User.create({ name, lname, email, password: hash_password });
+
     console.log(data)
     return data;
   }
 };
 
+
+
 export const login = async (body) => {
 
   const data = await User.findOne({ email: body.email });
+
   if (data != null) {
-    if (body.password == data.password) {
+    if
+      // (body.password == data.password)
+      (bcrypt.compare(body.password, data.password)) {
       return data
     } else {
       // return "Check your pass and email"
