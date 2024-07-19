6bb32666a497d13d819e2616fbed575cadc11520
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-07-19 23:13:45 +0530

    [jayesh]Update:remove the unwanted clg

--- fundoonotes/src/middlewares/auth.middleware.js
+++ fundoonotes/src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import HttpStatus from 'http-status-codes';
 import jwt from 'jsonwebtoken';
-import { Http } from 'winston/lib/winston/transports';
+// import { Http } from 'winston/lib/winston/transports';
 
 /**
  * Middleware to authenticate if user has a valid Authorization token
@@ -18,16 +18,21 @@ export const userAuth = async (req, res, next) => {
         code: HttpStatus.BAD_REQUEST,
         message: 'Authorization token is required'
       };
+    // console.log("bereaer toekn befroe split ========>", bearerToken)
+
     bearerToken = bearerToken.split(' ')[1];
+    // console.log("bereaer toekn after split ========>", bearerToken)
 
     const user = await jwt.verify(bearerToken, process.env.hidden_key);
+    req.body.createdBy = user.userId
+    console.log(user.userId)
     // res.locals.user = user;
     // res.locals.token = bearerToken;
     console.log("decodeed data after auth! ============>", user)
     next();
   } catch (error) {
     // next(error);
-
+    // console.log("error from userrr auth =================>", error)
     res.status(HttpStatus.UNAUTHORIZED).json({
       code: HttpStatus.UNAUTHORIZED,
       message: `${error}`
