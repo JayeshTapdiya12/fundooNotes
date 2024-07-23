e396a56602e90e3784b48a06c520339675c407f5
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-07-23 11:48:49 +0530

    [jayesh]Update:added routing for the trash and archive

--- fundoonotes/src/routes/note.route.js
+++ fundoonotes/src/routes/note.route.js
@@ -14,6 +14,12 @@ router.get('', userAuth, NoteController.getAllNote);
 
 router.post('', noteValidator, userAuth, NoteController.addNote);
 
+//making for archived note
+
+router.post('/:_id/archived', userAuth, NoteController.noteArchive)
+
+// // making for trash
+router.post('/:_id/trash', userAuth, NoteController.trash)
 
 
 //get note by id
@@ -21,16 +27,16 @@ router.post('', noteValidator, userAuth, NoteController.addNote);
 router.get('/:_id', userAuth, NoteController.noteFind);
 
 
-
 //update data
 
 router.put('/:_id', noteValidator, userAuth, NoteController.noteUpdate);
 
 
-
 //delete data
 
 router.delete('/:_id', userAuth, NoteController.deleteNote);
+
+
 
 
 export default router;
