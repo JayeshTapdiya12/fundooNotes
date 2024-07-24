715aa61c363bb00787be2a9fc76e01b7e6c5a2a9
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-07-24 13:45:03 +0530

    []jayesh]refactor:addede reset password and forget password

--- fundoonotes/src/routes/user.route.js
+++ fundoonotes/src/routes/user.route.js
@@ -1,17 +1,24 @@
 import express from 'express';
 import * as userController from '../controllers/user.controller';
 import { newUserValidator } from '../validators/user.validator';
+import { resetPasswordValidator } from '../validators/password.validator'
 import { userAuth } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
-
+// get all data of user
 router.get('', userController.getData)
 
 //route to create a new user
 router.post('/sign', newUserValidator, userController.sign);
 
+// route to login
 router.post('/login', userController.login)
 
+// route for reset password
+router.post('/reset_password', resetPasswordValidator, userAuth, userController.resetPassword)
+
+// route for forget password
+router.post('/forget_password', userController.forgetPassword)
 
 export default router;
