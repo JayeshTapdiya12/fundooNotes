7802bfc186e29dca4ed4a7ce18b25e25d2c55ae5
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-07-27 15:47:01 +0530

    [jayesh]modify:made Authmiddle ware as two use by many routes

--- fundoonotes/src/middlewares/auth.middleware.js
+++ fundoonotes/src/middlewares/auth.middleware.js
@@ -10,30 +10,64 @@ import jwt from 'jsonwebtoken';
  * @param {Object} res
  * @param {Function} next
  */
-export const userAuth = async (req, res, next) => {
-  try {
-    let bearerToken = req.header('Authorization');
-    if (!bearerToken)
-      throw {
-        code: HttpStatus.BAD_REQUEST,
-        message: 'Authorization token is required'
-      };
-    // console.log("bereaer toekn befroe split ========>", bearerToken)
+// export const userAuth = async (req, res, next) => {
+//   try {
+//     let bearerToken = req.header('Authorization');
+//     if (!bearerToken)
+//       throw {
+//         code: HttpStatus.BAD_REQUEST,
+//         message: 'Authorization token is required'
+//       };
+//     // console.log("bereaer toekn befroe split ========>", bearerToken)
 
-    bearerToken = bearerToken.split(' ')[1];
-    // console.log("bereaer toekn after split ========>", bearerToken)
+//     bearerToken = bearerToken.split(' ')[1];
+//     // console.log("bereaer toekn after split ========>", bearerToken)
 
-    const user = await jwt.verify(bearerToken, process.env.hidden_key);
-    req.body.createdBy = user.userId
-    req.body.Email = user.Email
-    console.log(user.userId)
+//     const user = await jwt.verify(bearerToken, process.env.hidden_key);
+//     req.body.createdBy = user.userId
+//     req.body.Email = user.Email
+//     console.log(user.userId)
 
-    next();
-  } catch (error) {
+//     next();
+//   } catch (error) {
 
-    res.status(HttpStatus.UNAUTHORIZED).json({
-      code: HttpStatus.UNAUTHORIZED,
-      message: `${error}`
-    })
-  }
+//     res.status(HttpStatus.UNAUTHORIZED).json({
+//       code: HttpStatus.UNAUTHORIZED,
+//       message: `${error}`
+//     })
+//   }
+// };
+
+
+
+export const userAuth = (secretKey) => {
+  return async (req, res, next) => {
+    try {
+      let bearerToken = req.header('Authorization');
+      console.log('bearerToken before splitting----->', bearerToken);
+
+      if (!bearerToken) {
+        throw {
+          code: HttpStatus.BAD_REQUEST,
+          message: 'Authorization token is required'
+        };
+      }
+
+      bearerToken = bearerToken.split(' ')[1];
+      console.log('bearerToken after splitting---->', bearerToken);
+
+      let userDetails = jwt.verify(bearerToken, secretKey);
+      req.body.UserID = userDetails.id;
+      // req.body.userId = userDetails.userId
+      console.log(req.body.userID)
+      req.body.Email = userDetails.Email;
+      console.log(req.body.Email)
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
 };
+
+
+
