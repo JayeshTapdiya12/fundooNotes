0dd7c45d2501ffa09ea5e4bc3b2897f53b81aa61
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2024-07-27 15:47:27 +0530

    [jayesh]modify:addede nodemailer function in routes

--- fundoonotes/src/routes/user.route.js
+++ fundoonotes/src/routes/user.route.js
@@ -3,7 +3,7 @@ import * as userController from '../controllers/user.controller';
 import { newUserValidator } from '../validators/user.validator';
 import { resetPasswordValidator } from '../validators/password.validator'
 import { userAuth } from '../middlewares/auth.middleware';
-
+require('dotenv').config();
 const router = express.Router();
 
 // get all data of user
@@ -15,10 +15,12 @@ router.post('/sign', newUserValidator, userController.sign);
 // route to login
 router.post('/login', userController.login)
 
-// route for reset password
-router.post('/reset_password', resetPasswordValidator, userAuth, userController.resetPassword)
-
 // route for forget password
 router.post('/forget_password', userController.forgetPassword)
+
+// route for reset password
+router.post('/reset_password', resetPasswordValidator, userAuth(process.env.forget_key), userController.resetPassword)
+
+
 
 export default router;
