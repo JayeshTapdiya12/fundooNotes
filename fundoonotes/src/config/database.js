2b71bfe8af3fb200b9232f7ee6506a965659c2c3
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-03-18 11:12:56 +0530

    new updated porocess

--- fundoonotes/src/config/database.js
+++ fundoonotes/src/config/database.js
@@ -3,21 +3,24 @@ import logger from './logger';
 
 const database = async () => {
   try {
-    // Replace database value in the .env file with your database config url
     const DATABASE =
       process.env.NODE_ENV === 'test'
         ? process.env.DATABASE_TEST
         : process.env.DATABASE;
 
+    if (!DATABASE) {
+      throw new Error('Database connection string is missing.');
+    }
+
     await mongoose.connect(DATABASE, {
-      useFindAndModify: false,
-      useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
+
     logger.info('Connected to the database.');
   } catch (error) {
     logger.error('Could not connect to the database.', error);
   }
 };
+
 export default database;
